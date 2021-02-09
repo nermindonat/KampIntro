00ef23625793a27ed0eeb50c262557c64434fe43
nermingurses21 <nermingurses21@gmail.com>
2021-02-10 01:41:06 +0300

    kamp deneme

---
+++ Metotlar/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Metotlar
+{
+    class Program
+    {
+        // cs - onun bir c# dosyası olduğunu gösterir.
+        //Clean code - Best Practic(temiz kod yazma)
+        //cw 2 tab yapınca hızlıca "console.Writeline" yazar
+        static void Main(string[] args)
+        {
+            Urun urun1 = new Urun();
+            urun1.Adi = "Elma";
+            urun1.Fiyati = 15;
+            urun1.Aciklama = "Amasya";
+
+            Urun urun2 = new Urun();
+            urun2.Adi = "Karpuz";
+            urun2.Fiyati = 80;
+            urun2.Aciklama = "Diyarbakır Karpuzu";
+
+            Urun[] urunler = new Urun[] {urun1, urun2 };
+
+            foreach (Urun urun in urunler)
+            {
+                Console.WriteLine(urun.Adi);
+                Console.WriteLine(urun.Fiyati);
+                Console.WriteLine(urun.Aciklama);
+                Console.WriteLine("---------------");
+            }
+
+            Console.WriteLine("-------------Metotlar-------------");
+            
+            //encapsulation
+            SepetManager sepetManager = new SepetManager();     //instance - class örneği
+            sepetManager.Ekle(urun1);
+            sepetManager.Ekle(urun2);
+
+            sepetManager.Ekle2("Armut", "Yeşil armut", 12, 10);
+            sepetManager.Ekle2("Elma", "Yeşil elma", 12, 9);
+            sepetManager.Ekle2("Karpuz", "Diyarbakır karpuzu", 12, 8);
+        }
+    }
+}

e82fbb53a1e7bc68ddb08e4e219d2fa98934ba5a
nermingurses21 <nermingurses21@gmail.com>
2021-02-10 01:41:32 +0300

    deneme1

--- DegerVeReferansTipler/Program.cs
+++ DegerVeReferansTipler/Program.cs
@@ -17,8 +17,10 @@ namespace DegerVeReferansTipler
             int[] sayilar1 = new int[] {10,20,30 };
             int[] sayilar2 = new int[] { 100, 200, 300 };
             sayilar1 = sayilar2;
-            sayilar2[0] = 999;         //
+            sayilar2[0] = 999;         
             //sayilar1 ??------>999
+
+
 
 
             // int,float,decimal,double,bool=değer tip

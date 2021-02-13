6bb8e07746e3c4f786be491e34cb5b5a5da42ddd
nermingurses21 <nermingurses21@gmail.com>
2021-02-13 20:45:51 +0300

    20 Ocak ders sonu

---
+++ GenericsIntro/MyList.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericsIntro
+{
+    //burdaki T tip(type) ten geliyor. 
+    class MyList<T>
+    {
+        T[] items;
+        public MyList()              //ctor + tab tab yap bu yapı gelir. constructor:bir metottur. classın ismiyle aynıdır.
+        {
+            items = new T[0];       // new lediğin anda çalışan bloğa constructor denir. new lediğin anda array ı sıfırlıyorsun.
+        }
+        public void Add(T item)
+        {
+            //temparray=geçici dizi
+            T[] tempArray = items;
+            items = new T[items.Length+1];
+
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                items[i] = tempArray[i];
+            }
+
+            items[items.Length - 1] = item;
+        }
+    }
+}
---
+++ OOP1/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP1
+{
+    class Product
+    {           //snippet
+        public int Id { get; set; }
+        public int CategoryId { get; set; }
+        public string ProductName { get; set; }
+        public double UnitPrice { get; set; }  //ürün fiyat
+        public int UnitsInStock { get; set; }   //stok adedi
+    }
+}
